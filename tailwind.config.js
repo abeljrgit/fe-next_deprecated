bcd7fce1f8392f7b862257031aab884a1ff11da2
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-02-12 05:26:34 +0800

    🚑 fix: Initial Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+      colors: {
+        transparent: 'transparent',
+        primary: '#E5383B',
+        secondary: '#141301',
+      },
+      screens: {
+        xs: '360px',
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1440px',
+      },
+    },
   },
   plugins: [require('tw-elements/dist/plugin')],
 };
